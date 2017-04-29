d51c93e518661b8586ba722d019d6fa85fd906b7
Felipe Santos <fesntmail@gmail.com>
2017-04-29 00:53:10 -0300

    Refactor more portal aluno

--- app/scripts/portal_aluno/portal.js
+++ app/scripts/portal_aluno/portal.js
@@ -1,42 +1,52 @@
 window.addEventListener('load', function() {
-	var url = document.location.href;
-
-    if(url.indexOf('aluno.ufabc.edu.br/dados_pessoais') != -1) {
+    if (isIndexPortalAluno()) {
         toastr.info("Clique em <a href='https://aluno.ufabc.edu.br/fichas_individuais' style='color: #FFF !important;'>Ficha Individual</a> para atualizar suas informações!");
-    }
-
-    if(url.indexOf('aluno.ufabc.edu.br/fichas_individuais') != -1) {
+    } else if (isFichasIndividuaisPath()) {
         toastr.info('A mágica começa agora...');
         
-        var aluno = getEmailAluno();
-		clearAlunoStorage(aluno);
+		clearAlunoStorage(getEmailAluno());
 
-		iterateCursosTable ();
+		iterateTabelaCursosAndSaveToLocalStorage();
     };
 });
 
-function iterateCursosTable () {
-	var cursos = $('tbody').children().slice(1);
-    cursos.each(function () {
-    	var curso = {};
-
-    	var tableRow = $(this).children();
-
-    	curso.curso = tableRow[0].innerText.replace("Novo", '');
-		curso.turno = tableRow[3].innerText;
-
-    	var link = $(tableRow[1]).children('a').attr('href');
-    	
-    	getFichaAluno(link, curso);
-    })
+function isIndexPortalAluno () {
+	return document.location.href
+		.indexOf('aluno.ufabc.edu.br/dados_pessoais') !== -1;
 }
 
-function getFichaAluno(url, curso) {
-    var ficha_url = url.replace('.json', '');
-    var aluno = getEmailAluno();
+function isFichasIndividuaisPath () {
+	return document.location.href
+		.indexOf('aluno.ufabc.edu.br/fichas_individuais') !== -1;
+}
 
-    $.get( 'https://aluno.ufabc.edu.br' + ficha_url, function( data ) {
+function iterateTabelaCursosAndSaveToLocalStorage () {
+	var aluno = getEmailAluno();
+
+	var tabelaCursos = $('tbody').children().slice(1);
+
+    tabelaCursos.each(function () {
+    	var linhaCurso = $(this).children();
+
+    	var fichaAlunoUrl = $(linhaCurso[1]).children('a').attr('href');
+    	
+    	getFichaAluno(fichaAlunoUrl, function(curso) {
+    		curso.curso = linhaCurso[0].innerText.replace("Novo", '');
+			curso.turno = linhaCurso[3].innerText;
+
+    		saveToLocalStorage(aluno, curso);
+    	});
+    });
+}
+
+function getFichaAluno(fichaAlunoUrl, cb) {
+	var curso = {};
+
+    var ficha_url = fichaAlunoUrl.replace('.json', '');
+
+    $.get('https://aluno.ufabc.edu.br' + ficha_url, function(data) {
         var ficha_obj = $($.parseHTML(data));
+
         var info = ficha_obj.find('.coeficientes tbody tr td');
 
         curso.cp = toNumber(info[0]);
@@ -44,17 +54,9 @@ function getFichaAluno(url, curso) {
         curso.ca = toNumber(info[2]);
         curso.quads = ficha_obj.find(".ano_periodo").length;
 
-        $.get( 'https://aluno.ufabc.edu.br' + url, function( data ) {
+        $.get( 'https://aluno.ufabc.edu.br' + fichaAlunoUrl, function(data) {
             curso.cursadas = data;
-            
-
-            chrome.storage.local.get(aluno, function (data) {
-                data[aluno].push(curso);
-                console.log(data);
-            	chrome.storage.local.set(data);
-             })
-            
-            toastr.info('Salvando disciplinas do curso do ' + curso.curso + ' para o usuário ' + aluno + '.');
+            cb(curso);
         });
     });   
 }
@@ -66,7 +68,7 @@ function getEmailAluno() {
 		.replace(/\s*/,'')
 		.split('|')[0]
 		.replace(' ','');
-};
+}
 
 function clearAlunoStorage(aluno) {
 	var tmp_obj = {};
@@ -76,4 +78,13 @@ function clearAlunoStorage(aluno) {
 
 function toNumber(el) {
 	return parseFloat(el.innerText.replace(',', '.'));
+}
+
+function saveToLocalStorage(aluno, curso) {
+	chrome.storage.local.get(aluno, function (data) {
+	    data[aluno].push(curso);
+	    console.log(data);
+		chrome.storage.local.set(data);
+		toastr.info('Salvando disciplinas do curso do ' + curso.curso + ' para o usuário ' + aluno + '.');
+	})
 }
